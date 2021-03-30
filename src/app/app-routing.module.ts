4199e23005e1a2198c3bfe70e12180df70d7f694
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-03-30 14:49:08 -0300

    re routeando la app para usar lazy loading por modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { BrandComponent } from './brand/brand.component';
+import { NavComponent } from './nav/nav.component';
 import { StockComponent } from './stock/stock.component';
 
 const routes: Routes = [
-
-  { path: 'brands', component: BrandComponent},
-  { path: 'about', component: AboutUsComponent },
-  { path: 'search', component: StockComponent }
+  { path: '',
+    component: NavComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/about',
+        pathMatch: 'full',
+      },
+      {
+        path: 'about',
+        loadChildren: () => import('./about-us/about-us.module').then(m => m.AboutUsModule)
+      },
+    ]
+  }
 ];
 
 @NgModule({
